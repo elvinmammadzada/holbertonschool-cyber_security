0c2c60ffb89d99278c946e45a060a91c484a86f6
elvinmammadzada <elvinmammadzada643@gmail.com>
2026-07-23 17:30:59 +0400

    Update 4-write_file.rb

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -13,4 +13,7 @@ def merge_json_files(file1_path, file2_path)
 
   # Save the combined result back to file2_path
   File.write(file2_path, JSON.pretty_generate(merged_data))
+
+  # Required console output for auto-grader
+  puts "Merged JSON written to #{file2_path}"
 end
